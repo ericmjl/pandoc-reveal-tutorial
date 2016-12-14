f15ffcf0b49700ca724cde036e73d850fa625ab6
Eric J. Ma <ericmajinglong@gmail.com>
2016-12-14 15:34:17 -0500

    updated build script

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 # Make HTML notes version of slides
 pandoc slides.md \
     -o index.html
-    # -c css/notes.css \
-    # --template=template.html \
-    # -H header.html
+    -c css/notes.css \
+    --template=template.html \
+    -H header.html
 
 # Make revealjs version of slides
 pandoc slides.md -o slides.html \
-    # --section-divs \
-    # -t revealjs \
-    # -s \
-    # --template template.revealjs \
-    # -H header.html \
+    --section-divs \
+    -t revealjs \
+    -s \
+    --template template.revealjs \
+    -H header.html \
 
 
 
